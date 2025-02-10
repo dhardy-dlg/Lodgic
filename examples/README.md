b243033a4cee77db8a20d1dcbaab66722958163b
dhardy <dhardy@novaa-tech.com>
2025-02-10 16:34:53 +0100

    feat: dev-202 Create the structure of librairy
    BREAKING CHANGE: This is the first breaking changes

---
+++ examples/README.md
@@ -0,0 +1,29 @@
+# Examples
+
+## Add
+
+```typescript
+import { add } from "../lib/utils";
+add(1, 2);
+```
+
+## Substract
+
+```typescript
+import { subtract } from "../lib/utils";
+subtract(4, 4);
+```
+
+## Multiply
+
+```typescript
+import { multiply } from "../lib/utils";
+multiply(4, 4);
+```
+
+## Divide
+
+```typescript
+import { divide } from "../lib/utils";
+divide(4, 4);
+```
---
+++ test/utils.test.ts
@@ -0,0 +1,19 @@
+import { expect, test } from "vitest";
+
+import { add, subtract, multiply, divide } from "../lib/utils";
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+test("substract 5 - 2 to equal 3", () => {
+  expect(subtract(5, 2)).toBe(3);
+});
+
+test("multiply 5 * 2 to equal 10", () => {
+  expect(multiply(5, 2)).toBe(10);
+});
+
+test("divide 10 / 2 to equal 5", () => {
+  expect(divide(10, 2)).toBe(5);
+});
